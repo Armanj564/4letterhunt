3e9d28d4b24db6530f9e65027931e21ab6df47d7
Unknown <qarmanj74@gmail.com>
2026-04-15 14:42:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN print("TOKEN =", TOKEN)
 
 # ── /start ──────────────────────────────────────────────
 async def start(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
